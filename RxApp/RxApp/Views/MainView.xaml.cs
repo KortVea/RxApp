2de9aee7f21f4ac8b62b0842275b43b71e8c1302
Isaah Liu <isaah.liu@purple.telstra.com>
2022-08-11 16:58:33 +1000

    basic

--- RxApp/RxApp/Views/MainView.xaml.cs
+++ RxApp/RxApp/Views/MainView.xaml.cs
@@ -1,10 +1,35 @@
-﻿namespace RxApp.Views
+﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveMarbles.ObservableEvents;
+using ReactiveUI;
+using Xamarin.Forms;
+
+namespace RxApp.Views
 {
     public partial class MainView
     {
         public MainView()
         {
             InitializeComponent();
+
+            
+            
+            this.WhenActivated(d =>
+            {
+                Observable
+                    .FromEventPattern<TextChangedEventArgs>(
+                        h => this.Entry.TextChanged += h,
+                        h => this.Entry.TextChanged -= h)
+                    .Select(arg => arg.EventArgs.NewTextValue)
+                    .BindTo(this.ViewModel, vm => vm.EntryValue)
+                    .DisposeWith(d);
+
+                this
+                    .Bind(this.ViewModel, vm => vm.SliderValue, v => v.Slider.Value)
+                    .DisposeWith(d);
+                
+                
+            });
         }
     }
 }

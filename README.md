c68075264760f7854680d6915cb5c529d3c5ef5a
Isaah Liu <isaah.liu@purple.telstra.com>
2022-08-23 00:29:07 +1000

    readme

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 ## Quiz
 
-Implement a secrect lock whereby
+Implement a secret lock whereby
 - the moment the toggle is toggled on, 
     - if the text entry is "hey"
     - if the slider is greater than 0.5,
 - the button would be enabled for 5 sec.
+
+
+
+Hints:
+- ReactiveCommand.CreateFromObservable can take be bound to a UI action via ICommand interface.
+- It also takes a CanExecute observable
+  
---
+++ RxApp.ViewModels/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using ReactiveUI;
+using Sextant;
+
+namespace ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private double sliderValue = 0;
+        private string entryValue = string.Empty;
+        
+        public MainViewModel(IViewStackService viewStackService, IScheduler mainScheduler) : base(viewStackService)
+        {
+            this
+                .WhenAnyValue(x => x.SliderValue)
+                .Throttle(TimeSpan.FromSeconds(0.5))
+                .Subscribe(Console.WriteLine);
+
+            this
+                .WhenAnyValue(x => x.EntryValue)
+                .Subscribe(Console.WriteLine);
+            
+            
+
+            this.ButtonClickedCommand =
+                ReactiveCommand
+                    .CreateFromObservable(
+                        execute: () => ViewStackService.PushPage<ResultModalViewModel>(), 
+                        canExecute: Observable.Return(true),
+                        outputScheduler: mainScheduler);
+            
+        }        
+        
+        public override string Id => nameof(MainViewModel);
+        
+        public ReactiveCommand<Unit, Unit> ButtonClickedCommand { get; set; }
+
+        public double SliderValue
+        {
+            get => this.sliderValue;
+            set => this.RaiseAndSetIfChanged(ref this.sliderValue, value);
+        }
+        
+        public string EntryValue
+        {
+            get => this.entryValue;
+            set => this.RaiseAndSetIfChanged(ref this.entryValue, value);
+        }
+    }
+}
